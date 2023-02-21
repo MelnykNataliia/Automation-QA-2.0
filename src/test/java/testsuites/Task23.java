61a41ddabebf78e4d11f1185d3ff853d0c7069d6
MelnykNataliia <melnyknataliiaiv@gmail.com>
2023-02-22 01:37:33 +0200

    Task 23. Created Test Suite

---
+++ src/test/java/testsuites/Task23.java
@@ -0,0 +1,28 @@
+package testsuites;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+import tests.*;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({
+		Task5.class,
+		Task6.class,
+		Task7.class,
+		Task8.class,
+		Task9.class,
+		Task10.class,
+		Task11.class,
+		Task12.class,
+		Task13.class,
+		Task14.class,
+		Task18.class,
+		Task19.class,
+		Task21.class,
+		Task22.class
+})
+
+public class Task23 {
+
+}
